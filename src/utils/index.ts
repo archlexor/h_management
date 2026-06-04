b35bfbb4aa8a07c482a58c237c34f09290eb7461
Shishir Joshi <163725706+Shishirjoshi@users.noreply.github.com>
2026-06-05 00:01:17 +0545

    feat: add shared currency helper

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export const formatCurrency = (amount: number) =>
+  new Intl.NumberFormat("en-US", {
+    currency: "USD",
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+    style: "currency",
+  }).format(amount);
